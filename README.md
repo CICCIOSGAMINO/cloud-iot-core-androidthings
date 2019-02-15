5c2beea4f9a2cbbf74bd1c9eecc292bb09588e15
@cicciosgamino <marco.canali@gmail.com>
2019-02-15 17:55:17 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# cloud-iot-core-androidthings
-Cloud IoT Core Client for Android Things (Kotlin)
+Cloud IoT Core Androidthings
+============================
+[TOC]
+
+The Cloud IoT Core Client makes it simple to integrate Android Things with Cloud IoT Core by providing abstractions for all of Cloud IoT Core's device functions:
+
++ Publishing telemetry events
++ Publishing device state
++ Receiving device configuration from Cloud IoT Core
++ Receiving device commands from Cloud IoT Core
+
+The library handles all concurrency so clients can use it in the main thread. It also manages interactions with the Cloud IoT Core MQTT bridge so users don't have to worry about low-level networking protocols.
+
+This is the Kotlin version ... follow the Official Link 
+
+https://github.com/androidthings/cloud-iot-core-androidthings
---
+++ app/src/main/java/com/ciccio/iotcoreclient/IotCoreClient.kt
@@ -0,0 +1,93 @@
+package com.ciccio.iotcoreclient
+
+import com.google.android.things.bluetooth.ConnectionParams
+import java.util.concurrent.atomic.AtomicBoolean
+
+/**
+ * IotCoreClient manages interactions with Google Cloud IoT Core for a single device.
+ *
+ * <p>This class provides mechanisms for using Cloud IoT Core's main features. Namely
+ * <ul>
+ *    <li>Publishing device telemetry</li>
+ *    <li>Publishing device state</li>
+ *    <li>Receiving configuration changes</li>
+ *    <li>Receiving commands</li>
+ * </ul>
+ *
+ * <p>Create a new IotCoreClient using the {@link IotCoreClient.Builder}, and call
+ * {@link IotCoreClient#connect()} to initialize the client connection. When you no longer
+ * need to send and receive data, call {@link IotCoreClient#disconnect()} to close the connection
+ * and free up resources.
+ *
+ * <p>Track the current connection using {@link IotCoreClient#isConnected()} or register a
+ * {@link ConnectionCallback} to listen for changes in the client's conn, and publish data to Cloud
+ *
+ * <p>Publish data to Cloud IoT Core using {@link IotCoreClient#publishTelemetry(TelemetryEvent)}
+ * and {@link IotCoreClient#publishDeviceState(byte[])}. These methods can be used regardless of the
+ * client's connection state. If the client is connected, messages are published immediately.
+ * Otherwise, if the client is disconnected, messages are stored in memory and sent when the
+ * connection is reestablished.
+ *
+ * <p>Register an {@link OnConfigurationListener} with the client to receive device configuration
+ * changes from Cloud IoT Core.
+ *
+ * <pre class="prettyprint">
+ *     IotCoreClient iotCoreClient = new IotCoreClient.Builder()
+ *             .setConnectionParams(connectionParams)
+ *             .setKeyPair(keyPair);
+ *             .setOnConfigurationListener(onConfigurationListener)
+ *             .setOnCommandListener(onCommandListener)
+ *             .setConnectionCallback(connectionCallback)
+ *             .build();
+ *     iotCoreClient.connect();
+ *     iotCoreClient.publishDeviceState("Hello world!".getBytes());
+ * </pre>
+ *
+ * <p>While disconnected, the client queues all messages for delivery when the connection is
+ * restored. To customize the behavior of the offline message queue, call
+ * {@link IotCoreClient.Builder#setTelemetryQueue(Queue)} with a queue implementation suited to
+ * your application. If no queue implementation is provided, the default queue implementation is a
+ * queue that stores up to 1000 telemetry events and drops events from the head of the queue when
+ * messages are inserted beyond the maximum capacity.
+ */
+
+private val TAG = IotCoreClient::class.java.simpleName
+
+class IotCoreClient {
+
+    // Settings for exponential backoff behavior. These values are from Cloud IoT Core's recommendations at
+    // https://cloud.google.com/iot/docs/requirements#managing_excessive_load_with_exponential_backoff
+    private val INITIAL_RETRY_INTERVAL_MS: Long = 1000
+    private val MAX_RETRY_JITTER_MS = 1000
+    private val MAX_RETRY_INTERVAL_MS = (64 * 1000).toLong()
+
+    // Default telemetry queue capacity
+    private val DEFAULT_QUEUE_CAPACITY = 1000
+
+    // Quality of service level ( 1 = at least one / 0 = at most one )
+    private val QOS_FOR_DEVICE_STATE_MESSAGES = 1
+
+    // All necessary info to connect to Cloud IoT Core
+    private lateinit var mConnectionParams : ConnectionParams
+
+    // Generates signed JWTs to authenticate with Cloud IoT Core
+    private lateinit var mJwtGenerator : JwtGenerator
+
+    // Underlying MQTT client implementation.
+    private lateinit var mMqttClient : MqttClient
+
+    // Subscriptions Topics
+    private var mSubscriptionTopics = listOf<String>()
+
+    // Control the execution of background thred, The thread stops if mRunBackgroundThread is
+    // false
+    private lateinit var mRunBackgroundThread : AtomicBoolean
+
+    // Store telemetry events that failed to send so it can be resent when connection
+    // to IoT Core can be reestablished
+    private lateinit var mUnsentTelemetryEvent : TelemetryEvent
+
+
+
+
+}
